cd9876f20829adc3d17847436da38b86bd4145d3
Denise <dtmu1974@gmail.com>
2026-04-04 19:05:38 -0700

    remove file

--- tests/ddo.spec.ts
+++ tests/ddo.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test';
+import * as path from 'path';
+import { pathToFileURL } from 'url';
 
 test('birthday card generator', async ({ page }) => {
   test.setTimeout(60000); // 60 second timeout for this test
   //await page.goto('file:///c:/temp/playwright/tests/data/index.html');
-  // 1. Get the Absolute Path dynamically
-  // process.cwd() = The root of your project (TestProject)
-  const filePath = path.resolve(process.cwd(), 'tests/data/index.html');
-  
-  // 2. Convert it to a File URL
-  const fileUrl = `file://${filePath}`;
-  
+  // 1. Get the absolute path dynamically
+  const filePath = path.resolve(__dirname, 'data/index.html');
+
+  // 2. Convert it to a file URL
+  const fileUrl = pathToFileURL(filePath).href;
+
   // 3. Navigate
   await page.goto(fileUrl);
 
